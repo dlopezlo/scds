e45c7b8111ce9ba942815f20c85f698638ab3d3a
Daniel López <dlopezlo@gmail.com>
2024-05-12 11:31:07 +0200

    cosmetic changes

--- src/main.go
+++ src/main.go
@@ -7,14 +7,16 @@ import (
 	"github.com/dlopezlo/scds/server"
 )
 
+const APP_PORT = ":3001"
+
 func main() {
+
 	s := server.CreateNewServer()
 	s.MountHandlers()
-	const APP_PORT = ":3000"
 
+	log.Printf("Starting server on port %s", APP_PORT)
 	err := http.ListenAndServe(APP_PORT, s.Router)
 	if err != nil {
 		log.Printf("Error starting server: %s\n", err)
 	}
-	log.Printf("Server ready on port %s", APP_PORT)
 }
