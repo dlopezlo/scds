e2ef643cdd3f55563738f7872221629ab1d93bfe
Daniel López <dlopezlo@gmail.com>
2024-05-12 11:25:37 +0200

    move code to package

--- src/main.go
+++ src/main.go
@@ -1,107 +1,20 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
-	"os"
-	"text/template"
-	"time"
 
-	"github.com/dlopezlo/scds/utils"
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"github.com/dlopezlo/scds/server"
 )
 
-type Element struct {
-	Host         []string `json:"host"`
-	Hostname     string   `json:"hostname"`
-	Port         int      `json:"port,omitempty"`
-	ProxyCommand string   `json:"proxyCommand,omitempty"`
-	ProxyJump    string   `json:"proxyjump,omitempty"`
-	User         string   `json:"user,omitempty"`
-}
-
 func main() {
-	log.SetOutput(os.Stdout)
-	s := CreateNewServer()
+	s := server.CreateNewServer()
 	s.MountHandlers()
+	const APP_PORT = ":3000"
 
-	err := http.ListenAndServe(":3000", s.Router)
+	err := http.ListenAndServe(APP_PORT, s.Router)
 	if err != nil {
 		log.Printf("Error starting server: %s\n", err)
 	}
-	log.Println("Server ready on port 3000")
-}
-
-type Server struct {
-	Router *chi.Mux
-}
-
-func CreateNewServer() *Server {
-	s := &Server{}
-	s.Router = chi.NewRouter()
-	return s
-}
-
-func (s *Server) MountHandlers() {
-	// Middlewares
-	s.Router.Use(middleware.Logger)
-	s.Router.Use(middleware.Timeout(time.Second * 3))
-	// handlers
-	s.Router.Get("/", WelcomeHandler)
-	s.Router.Get("/sshconfig/{username}", getSSHConfigHandler)
-}
-
-func WelcomeHandler(w http.ResponseWriter, r *http.Request) {
-	_, err := w.Write([]byte("Welcome to my TFG"))
-	if err != nil {
-		log.Println("Error writing response:", err)
-	}
-}
-
-func getSSHConfigHandler(w http.ResponseWriter, r *http.Request) {
-	var data struct {
-		Hosts    []Element `json:"hosts"`
-		Username string
-	}
-	// grab username and make some sanity checks
-	username, err := utils.GetUsername(chi.URLParam(r, "username"))
-
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	data.Username = username
-	// read the file with the config in json format
-	sshData, err := os.ReadFile("ssh-data.json")
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		log.Println("Error reading data file:", err)
-		return
-	}
-
-	// Read the byts into an object
-	err = json.Unmarshal(sshData, &data)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		log.Println("Error parsing json:", err)
-		return
-	}
-
-	// load the template
-	tpl, err := template.New("template.tpl").ParseFiles("template.tpl")
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		log.Println("Error with templating system:", err)
-	}
-
-	// Execute renders the template with the data and writes to
-	// responseWrite back to the user.
-	err = tpl.Execute(w, data)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		log.Println("Error with templating system:", err)
-	}
+	log.Printf("Server ready on port %s", APP_PORT)
 }
--- src/server/server.go
+++ src/server/server.go
@@ -1,1 +1,95 @@
 package server
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+	"text/template"
+	"time"
+
+	"github.com/dlopezlo/scds/utils"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+type Element struct {
+	Host         []string `json:"host"`
+	Hostname     string   `json:"hostname"`
+	Port         int      `json:"port,omitempty"`
+	ProxyCommand string   `json:"proxyCommand,omitempty"`
+	ProxyJump    string   `json:"proxyjump,omitempty"`
+	User         string   `json:"user,omitempty"`
+}
+
+type Server struct {
+	Router *chi.Mux
+}
+
+func CreateNewServer() *Server {
+	s := &Server{}
+	s.Router = chi.NewRouter()
+	return s
+}
+
+func (s *Server) MountHandlers() {
+	// Middlewares
+	s.Router.Use(middleware.Logger)
+	s.Router.Use(middleware.Timeout(time.Second * 3))
+	// handlers
+	s.Router.Get("/", WelcomeHandler)
+	s.Router.Get("/sshconfig/{username}", getSSHConfigHandler)
+}
+
+func WelcomeHandler(w http.ResponseWriter, r *http.Request) {
+	_, err := w.Write([]byte("Welcome to my TFG"))
+	if err != nil {
+		log.Println("Error writing response:", err)
+	}
+}
+
+func getSSHConfigHandler(w http.ResponseWriter, r *http.Request) {
+	var data struct {
+		Hosts    []Element `json:"hosts"`
+		Username string
+	}
+	// grab username and make some sanity checks
+	username, err := utils.GetUsername(chi.URLParam(r, "username"))
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	data.Username = username
+	// read the file with the config in json format
+	sshData, err := os.ReadFile("ssh-data.json")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("Error reading data file:", err)
+		return
+	}
+
+	// Read the byts into an object
+	err = json.Unmarshal(sshData, &data)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("Error parsing json:", err)
+		return
+	}
+
+	// load the template
+	tpl, err := template.New("template.tpl").ParseFiles("template.tpl")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("Error with templating system:", err)
+	}
+
+	// Execute renders the template with the data and writes to
+	// responseWrite back to the user.
+	err = tpl.Execute(w, data)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("Error with templating system:", err)
+	}
+}
