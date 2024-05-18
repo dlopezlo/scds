a15e2933ce7c23d5bf610a784c751e57a12c963b
Daniel López <dlopezlo@gmail.com>
2024-05-18 16:14:58 +0200

    update helm files

--- devops/infra/providers.tf
+++ devops/infra/providers.tf
@@ -6,5 +6,9 @@ terraform {
             source = "BarnabyShearer/dockerhub"
             version = ">= 0.0.15"
         }
+        digitalocean = {
+            source = "digitalocean/digitalocean"
+            version = "~> 2.0"
+        }
     }
 }
