fd5178d945a3fbdf338ed1f5a7259b199cf98c7d
Daniel López <dlopezlo@gmail.com>
2024-05-20 19:58:33 +0200

    cosmetic change, test version update

--- src/main.go
+++ src/main.go
@@ -14,7 +14,7 @@ func main() {
 	s := server.CreateNewServer()
 	s.MountHandlers()
 
-	log.Printf("Starting server on port %s", APP_PORT)
+  log.Printf("Starting server on port: %s", APP_PORT)
 	err := http.ListenAndServe(APP_PORT, s.Router)
 	if err != nil {
 		log.Printf("Error starting server: %s\n", err)
